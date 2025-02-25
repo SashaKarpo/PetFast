e6116496a8809ac2d230200b6400cc2ef6dd5a34
Саша <prmzwe@gmail.com>
2025-02-25 17:15:22 +0300

    added add_task and classes STaskAdd and STask

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class STaskAdd(BaseModel):
+    name: str
+    description: str | None
+
+
+class STask(STaskAdd):
+    id: int
+
+
+tasks = []
+
+
+@app.post("/tasks")
+async def add_task(task: Annotated[STaskAdd, Depends()]):
+    tasks.append(task)
+    return {'ok': True}
+
+# @app.get('/tasks')
+# def get_tasks():
+#     task = Task(name='Запиши это слово')
+#     return {'data': task}
